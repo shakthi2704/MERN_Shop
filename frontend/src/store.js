a5d3b85c4437113c0be03337da47906dbaa64b8c
shakthi2704 <59468802+shakthi2704@users.noreply.github.com>
2021-01-11 15:31:01 +0530

    Product details reducer & action
    Product details reducer & action

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,8 +1,34 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
+import {
+  productListReducer,
+  productDetailsReducer,
+} from './reducers/productReducers'
 
-const reducer = combineReducers({})
+const reducer = combineReducers({
+  productList: productListReducer,
+  productDetails: productDetailsReducer,
+  // productDelete: productDeleteReducer,
+  // productCreate: productCreateReducer,
+  // productUpdate: productUpdateReducer,
+  // productReviewCreate: productReviewCreateReducer,
+  // productTopRated: productTopRatedReducer,
+  // cart: cartReducer,
+  // userLogin: userLoginReducer,
+  // userRegister: userRegisterReducer,
+  // userDetails: userDetailsReducer,
+  // userUpdateProfile: userUpdateProfileReducer,
+  // userList: userListReducer,
+  // userDelete: userDeleteReducer,
+  // userUpdate: userUpdateReducer,
+  // orderCreate: orderCreateReducer,
+  // orderDetails: orderDetailsReducer,
+  // orderPay: orderPayReducer,
+  // orderDeliver: orderDeliverReducer,
+  // orderListMy: orderListMyReducer,
+  // orderList: orderListReducer,
+})
 
 const initialState = {}
 
