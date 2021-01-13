4831bdf24d8c8b9c7b2d4b90985f1c05b8b02c2a
shakthi2704 <59468802+shakthi2704@users.noreply.github.com>
2021-01-13 11:10:43 +0530

    final
    final

--- backend/seeder.js
+++ backend/seeder.js
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
-// import colors from 'colors'
+import colors from 'colors'
 import users from './data/users.js'
 import products from './data/products.js'
 import User from './models/userModel.js'
@@ -28,26 +28,28 @@ const importData = async () => {
 
     await Product.insertMany(sampleProducts)
 
-    console.log('Data Imported!')
+    console.log('Data Imported!'.green.inverse)
     process.exit()
   } catch (error) {
-    console.error(`${error}`)
+    console.error(`${error}`.red.inverse)
     process.exit(1)
   }
 }
+
 const destroyData = async () => {
   try {
     await Order.deleteMany()
     await Product.deleteMany()
     await User.deleteMany()
 
-    console.log('Data Destroyed!')
+    console.log('Data Destroyed!'.red.inverse)
     process.exit()
   } catch (error) {
-    console.error(`${error}`)
+    console.error(`${error}`.red.inverse)
     process.exit(1)
   }
 }
+
 if (process.argv[2] === '-d') {
   destroyData()
 } else {
